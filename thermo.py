d6eaf0515a1b0573291304e5eb53f9f30d2938b3
acyang <acyang0903@gmail.com>
2018-10-31 18:07:30 +0800

    Add files via upload
    use subplot to combine  temperature and pressure in one figure

--- thermo.py
+++ thermo.py
@@ -1,9 +1,8 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-plt.figure(dpi=300)
-axes = plt.subplots(2, 1)
-
+#Prepare data
 time = []
 temp = []
 press = []
@@ -19,17 +18,50 @@ with open('tdo.txt','r') as csvfile:
         press.append(float(row[3]))
         etotal.append(float(row[8]))
 
-#plt.plot(time,temp, color='#00aa00', label='Line 1')
-plt.plot(time,press, color='#ff007f', label='Line 2')
+
+#plt.figure(dpi=300)
+fig, axes = plt.subplots(2, 1)
+axes[0].plot(time,temp, color='#00aa00', label='Line 1')
+axes[1].plot(time,press, color='#ff007f', label='Line 2')
 #plt.plot(time,etotal, color='#bc007f', label='Line 3')
 
-plt.legend(loc=2, title='abc', frameon=False)
-plt.title('time v.s. temperature')
-plt.axis([0, 200000, 260, 340])
-plt.xlabel('time')
-plt.ylabel('temperature (K)')
-plt.grid(True, color='#ECE5DE', linestyle='solid')
-plt.tick_params(axis='x', bottom=True, top=True)
-plt.tick_params(axis='y', left=True, right=True)
+#axes[0].legend(loc=2, title='abc', frameon=False)
+#axes[0].title('time v.s. temperature')
+axes[0].axis([0, 200000, 260, 340])
+#axes[0].set_xlabel('time')
+axes[0].set_ylabel('temperature (K)')
+axes[0].grid(True, color='#ECE5DE', linestyle='solid')
+axes[0].tick_params(axis='x', bottom=True, top=True)
+axes[0].tick_params(axis='y', left=True, right=True)
+
+#axes[1].legend(loc=2, title='abc', frameon=False)
+#axes[1].title('time v.s. temperature')
+axes[1].axis([0, 200000, -20000, 20000])
+axes[1].set_xlabel('time')
+axes[1].set_ylabel('presure (bars)')
+axes[1].grid(True, color='#ECE5DE', linestyle='solid')
+axes[1].tick_params(axis='x', bottom=True, top=True)
+axes[1].tick_params(axis='y', left=True, right=True)
+#plt.title('time v.s. temperature')
 plt.show()        
 plt.savefig('time-thermo.png',dpi=300)
+
+
+#
+#fig, ax1 = plt.subplots()
+#
+#color = 'tab:red'
+#ax1.set_xlabel('time (s)')
+#ax1.set_ylabel('exp', color=color)
+#ax1.plot(t, data1, color=color)
+#ax1.tick_params(axis='y', labelcolor=color)
+#
+#ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+#
+#color = 'tab:blue'
+#ax2.set_ylabel('sin', color=color)  # we already handled the x-label with ax1
+#ax2.plot(t, data2, color=color)
+#ax2.tick_params(axis='y', labelcolor=color)
+#
+#fig.tight_layout()  # otherwise the right y-label is slightly clipped
+#plt.show()
